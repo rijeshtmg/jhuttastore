d07eb1da0fd9b7e22df153f490d73e6c1bb300ed
Rijesh Lopchan <rijeshlopchan@Rijeshs-MacBook-Pro.local>
2020-09-08 17:56:37 +0545

    Updates

---
+++ precache-manifest.4b836b3e4fc6446cc15bac100e1541ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a4834d542a3cf04796ab4dd39a42c7",
+    "url": "/jhuttastore/index.html"
+  },
+  {
+    "revision": "ab141ec7d2c18f165f22",
+    "url": "/jhuttastore/static/css/main.d8c7dfc0.chunk.css"
+  },
+  {
+    "revision": "7b749334cc97a8f86dda",
+    "url": "/jhuttastore/static/js/2.154e30dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jhuttastore/static/js/2.154e30dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab141ec7d2c18f165f22",
+    "url": "/jhuttastore/static/js/main.7d515d1c.chunk.js"
+  },
+  {
+    "revision": "4fa1258e40babba3dc67",
+    "url": "/jhuttastore/static/js/runtime-main.98a9e6ac.js"
+  },
+  {
+    "revision": "980b9f32a3f482b7db688fa8df0488cd",
+    "url": "/jhuttastore/static/media/Manrope.980b9f32.ttf"
+  },
+  {
+    "revision": "5cc9cd6dcfae2488d09a568376622569",
+    "url": "/jhuttastore/static/media/shoe.5cc9cd6d.png"
+  }
+]);
